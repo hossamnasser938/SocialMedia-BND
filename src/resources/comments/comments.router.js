26b99a29e7d0acb2cf84f33a821fe079079611b5
hossamnasser938 <hossamnasser938@gmail.com>
2022-03-06 08:41:58 +0200

    validate getPostComments and getComment

--- src/resources/comments/comments.router.js
+++ src/resources/comments/comments.router.js
@@ -1,18 +1,27 @@
 import { Router } from "express";
 import CommentsController from "./comments.controller";
 import { validationMiddleware } from "../../middlewares/validation";
-import { createCommentSchema } from "./comments.schema";
+import {
+  createCommentSchema,
+  getCommentSchema,
+  getPostCommentsSchema,
+} from "./comments.schema";
 
 const router = new Router();
 
 router
   .route("/")
-  .get(CommentsController.getPostComments)
+  .get(
+    validationMiddleware(getPostCommentsSchema),
+    CommentsController.getPostComments
+  )
   .post(
     validationMiddleware(createCommentSchema),
     CommentsController.createComment
   );
 
-router.route("/:id").get(CommentsController.getComment);
+router
+  .route("/:id")
+  .get(validationMiddleware(getCommentSchema), CommentsController.getComment);
 
 export default router;
--- src/resources/comments/comments.schema.js
+++ src/resources/comments/comments.schema.js
@@ -5,3 +5,10 @@ export const createCommentSchema = Joi.object({
   text: Joi.string().required(),
   postId: Joi.objectID(),
 });
+
+export const getPostCommentsSchema = Joi.object({
+  page: Joi.number(),
+  post_id: Joi.objectID(),
+});
+
+export const getCommentSchema = Joi.object({ id: Joi.objectID() });
